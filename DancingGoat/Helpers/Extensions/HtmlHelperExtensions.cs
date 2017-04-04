393f78e9746f877c1618ca11d03970bb7984e63f
Jan Lenoch <jan.lenoch@hotmail.cz>
2017-04-04 14:29:38 +0200

    DC-143: Demonstrate display templates, fixes #8, fixes #10

--- DancingGoat/Helpers/Extensions/HtmlHelperExtensions.cs
+++ DancingGoat/Helpers/Extensions/HtmlHelperExtensions.cs
@@ -13,20 +13,23 @@ namespace DancingGoat.Helpers.Extensions
         /// Generates an IMG tag for an image file.
         /// </summary>
         /// <param name="htmlHelper">HTML helper.</param>
-        /// <param name="asset">The asset to display</param>
+        /// <param name="assets">The asset sequence to display the asset from</param>
+        /// <param name="index">Index position of the asset</param>
         /// <param name="title">Title</param>
         /// <param name="cssClass">CSS class</param>
-        public static MvcHtmlString AssetImage(this HtmlHelper htmlHelper, Asset asset, string title = null, string cssClass = "", int? width = null, int? height = null)
+        /// <param name="width">Optional width size</param>
+        /// <param name="height">Optional height size</param>
+        public static MvcHtmlString AssetImage(this HtmlHelper htmlHelper, IEnumerable<Asset> assets, int index, string title = null, string cssClass = "", int? width = null, int? height = null)
         {
-            if (asset == null)
+            if (assets == null || !assets.Any() || assets.ElementAt(index) == null)
             {
                 return MvcHtmlString.Empty;
             }
 
             var image = new TagBuilder("img");
-            image.MergeAttribute("src", asset.Url);
+            image.MergeAttribute("src", assets.ElementAt(index).Url);
             image.AddCssClass(cssClass);
-            string titleToUse = title ?? asset.Name;
+            string titleToUse = title ?? assets.ElementAt(index).Name ?? string.Empty;
             image.MergeAttribute("alt", titleToUse);
             image.MergeAttribute("title", titleToUse);
 
@@ -43,6 +46,12 @@ namespace DancingGoat.Helpers.Extensions
             return MvcHtmlString.Create(image.ToString(TagRenderMode.SelfClosing));
         }
 
+        /// <summary>
+        /// Displays a <see cref="DateTime"/> in a formatted manner.
+        /// </summary>
+        /// <param name="htmlHelper">HTML helper.</param>
+        /// <param name="dateTime">The <see cref="DateTime"/> to format</param>
+        /// <param name="format">The formatting character</param>
         public static MvcHtmlString DateTimeFormatted(this HtmlHelper htmlHelper, DateTime? dateTime, string format)
         {
             if (!dateTime.HasValue || string.IsNullOrEmpty(format))
