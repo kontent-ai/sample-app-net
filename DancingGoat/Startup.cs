9a1ff4092cd4d6cd0e070cf127f28f2f33f26a73
Ales Kalina <alesk@kentico.com>
2017-05-02 08:21:43 +0200

    Set TLS 1.2 as the default potocol

--- DancingGoat/Startup.cs
+++ DancingGoat/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Net;
 
 [assembly: OwinStartupAttribute(typeof(DancingGoat.Startup))]
 
@@ -9,6 +10,8 @@ namespace DancingGoat
     {
         public void Configuration(IAppBuilder app)
         {
+            // .NET Framework 4.6.1 and lower does not support TLS 1.2 as the default protocol, but Delivery API requires it.
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
     }
 }
