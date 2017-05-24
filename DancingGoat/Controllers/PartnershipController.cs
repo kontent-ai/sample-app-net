8340905a6172fb598443c2a1c20d0dc487f84431
Jan Lenoch <jan.lenoch@hotmail.cz>
2017-05-24 12:26:26 +0200

    DC-151. Added comments to controllers that have forms.

--- DancingGoat/Controllers/PartnershipController.cs
+++ DancingGoat/Controllers/PartnershipController.cs
@@ -12,10 +12,14 @@ namespace DancingGoat.Controllers
             return View();
         }
         
+        /// <summary>
+        /// Dummy action; form information is being handed over to Kentico Cloud Engagement management service through JavaScript.
+        /// </summary>
         [Route]
         [HttpPost]
         public ActionResult Application()
         {
+            // If needed, put your code here to work with the uploaded data in MVC.
             TempData["formApplied"] = true;
             return RedirectToAction("Index");
         }
