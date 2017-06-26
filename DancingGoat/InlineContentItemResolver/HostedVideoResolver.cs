de4023b81f006e94a381653f0cb82b9ba4b6be65
Martin Knapik <knapik.martin92@gmail.com>
2017-06-26 19:14:04 +0200

    CMP-290 - fixed nitpicks

--- DancingGoat/InlineContentItemResolver/HostedVideoResolver.cs
+++ DancingGoat/InlineContentItemResolver/HostedVideoResolver.cs
@@ -14,13 +14,13 @@ namespace DancingGoat.InlineContentItemResolver
             }
             var selected = data.Item.VideoHost.First().Name;
             if (selected == "Vimeo")
-            {
                 return
-                    $"<div><iframe  src=\"https://player.vimeo.com/video/{data.Item.VideoId}?title=0&byline=0&portrait=0\"  width=\"640\" height=\"360\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><p></div>";
-            }
+                    $"<div><iframe src=\"https://player.vimeo.com/video/{data.Item.VideoId}?title=0&byline=0&portrait=0\" width=\"640\" " +
+                    $"height=\"360\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><p></div>";
             if (selected == "Youtube")
             {
-                return $"<iframe width=\"560\" height=\"315\" class=\"hosted-video__wrapper\" src=\"https://www.youtube.com/embed/{data.Item.VideoId}\" frameborder=\"0\" allowfullscreen></iframe>";
+                return $"<iframe width=\"560\" height=\"315\" class=\"hosted-video__wrapper\" " +
+                       $"src=\"https://www.youtube.com/embed/{data.Item.VideoId}\" frameborder=\"0\" allowfullscreen></iframe>";
             }
             return string.Empty;
         }
