9ed0711d7103e78f5b1bcc1864e9a77ffb29ffc7
Jan Lenoch <jan.lenoch@hotmail.cz>
2017-08-15 17:55:52 +0200

    Added percentage counter when deploying new project. DC-142

--- DancingGoat/Areas/Admin/Scripts/spinner.js
+++ DancingGoat/Areas/Admin/Scripts/spinner.js
@@ -1,6 +1,14 @@
 ﻿; (function ($, window, document, undefined) {
+    var spinnerSelector = "#spinner";
+    var maximumDuration = 120;
+    var intervalId = null;
+    var i = 1;
+
     $(document).ready(function () {
-        $("#spinner").bind("ajaxSend", function () {
+        $(spinnerSelector).bind("ajaxSend", function () {
+            if (intervalId !== null) {
+                clearInterval(intervalId);
+            }
             $(this).show();
         }).bind("ajaxStop", function () {
             $(this).hide();
@@ -12,8 +20,28 @@
     $(document).ready(function () {
         $("form").submit(function () {
             if ($(this).valid()) {
-                $('#spinner').show();
+                if (intervalId !== null) {
+                    clearInterval(intervalId);
+                }
+
+                $(spinnerSelector).show();
+
+                if ($(this)[0].id === "deploySample") {
+                    intervalId = startCounting(spinnerSelector, maximumDuration);
+                }
             }
         });
+    });
+
+    var startCounting = (function (spinner, durationSeconds) {
+        i = 1;
+        var interval = durationSeconds / 0.06;
+        var intervalId = setInterval(function () { setPercentage(spinner) }, interval);
+    });
+
+    var setPercentage = (function (spinner) {
+        var percentageText = i + " %";
+        $(spinner).children(".percentage").html(percentageText);
+        i++;
     });
 }(jQuery, window._spinner = window._spinner, window, document));
