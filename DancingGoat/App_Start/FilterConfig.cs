a1266559adb583db719b39c38ff2caee8daac083
Michal Paukert <MichalP2@kentico.com>
2018-06-12 07:54:48 +0200

    ET-11: ApplicationInsights

--- DancingGoat/App_Start/FilterConfig.cs
+++ DancingGoat/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace DancingGoat
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
