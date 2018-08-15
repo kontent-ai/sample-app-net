8d1e971d947a0c65e193af712b600b6db2e43520
Rosta Striz <rostislavs@kentico.com>
2018-08-15 22:13:25 +0200

    - code & projectId

--- DancingGoat/Controllers/ArticlesController.cs
+++ DancingGoat/Controllers/ArticlesController.cs
@@ -1,10 +1,11 @@
-﻿using DancingGoat.Models;
+﻿using System;
+using DancingGoat.Models;
 using KenticoCloud.Delivery;
-using System.Collections.Generic;
-using System.Net;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using KenticoCloud.Recommender.SDK;
 
 namespace DancingGoat.Controllers
 {
@@ -35,7 +36,29 @@ namespace DancingGoat.Controllers
             }
             else
             {
-                return View(response.Items[0]);
+                try
+                {
+                    var article = response.Items[0];
+
+                    var recommendationClient = new RecommendationClient("https://localhost:44342",
+                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJ1c3JfMHZPNHpxS2NiQ1dBdTdhMzM3ekxveiIsInBpZCI6IjNkN2MxMWU3LTEzNmItMDAzMS04OTljLTc5MTE5Zjg3MTdhMyIsImp0aSI6IklncFgzQlpINWVkWks0emsiLCJhdWQiOiJodHRwczovL2VuZ2FnZS1hcGkua2VudGljb2Nsb3VkLmNvbS8ifQ.YeDnsP7B8NGYn3dOSJGwu3x3o0QDjk6RinkWtGqiZ-o",
+                        2);
+
+                    var lastMonth = 24 * 60 * 60 * 30;
+
+                    var recommendedArticles = await recommendationClient.GetRecommendationsAsync(article.System.Codename, Request, Response, 
+                        filterQuery: "\"personas=Barista\" in 'properties'", 
+                        boosterQuery:$"if 'lastupdated' >= now() - {lastMonth} then 2 else 1", 
+                        limit: 2);
+                    var articles = (await client.GetItemsAsync<Article>(new InFilter("system.codename", recommendedArticles.Select(a => a.Codename).ToArray()))).Items;
+
+                    article.RelatedArticles = articles.Select(a => (object) a);
+                    return View(article);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
             }
         }
     }
