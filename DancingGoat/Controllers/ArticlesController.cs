d691ffa5a72fc8399fe37749c1e4df7e9b133b72
PetrŠvihlík <svihlik.petr@gmail.com>
2019-01-14 14:52:42 +0100

    approval changes

--- DancingGoat/Controllers/ArticlesController.cs
+++ DancingGoat/Controllers/ArticlesController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Configuration;
-using DancingGoat.Models;
+﻿using DancingGoat.Models;
 using KenticoCloud.Delivery;
+using KenticoCloud.Recommender;
+using KenticoCloud.Recommender.MVC;
+using System;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using KenticoCloud.Recommender;
-using KenticoCloud.Recommender.MVC;
 
 namespace DancingGoat.Controllers
 {
@@ -43,23 +43,27 @@ namespace DancingGoat.Controllers
                     var article = response.Items[0];
                     var recommendationApiKey = ConfigurationManager.AppSettings["RecommendationApiKey"];
 
-                    /* If there is a configuration key present */
-                    if (string.IsNullOrWhiteSpace(recommendationApiKey))
+                    // If the recommender API key is present
+                    if (!string.IsNullOrWhiteSpace(recommendationApiKey))
+                    {
+                        /* Get recommendations from the Recommendation engine */
+                        var recommendationClient = new RecommendationClient(recommendationApiKey, 5);
+                        var lastMonth = TimeSpan.FromDays(30).Milliseconds;
+
+                        var recommendedArticles = await recommendationClient
+                            .CreateRequest(Request, Response, codename: article.System.Codename, limit: 2, contentType: article.System.Type)
+                            //.WithFilterQuery("\"personas=Barista\" in 'properties'")
+                            //.WithBoosterQuery($"if 'lastupdated' >= now() - {lastMonth} then 2 else 1")
+                            .Execute();
+
+                        var articles = (await client.GetItemsAsync<Article>(new InFilter("system.codename", recommendedArticles.Select(a => a.Codename).ToArray()))).Items;
+                        article.RelatedArticles = articles.Select(a => (object)a);
                         return View(article);
-
-                    /* Get recommendations from the Recommendation engine */
-                    var recommendationClient = new RecommendationClient(recommendationApiKey, 5);
-                    var lastMonth = TimeSpan.FromDays(30).Milliseconds;
-
-                    var recommendedArticles = await recommendationClient
-                        .CreateRequest(Request, Response, codename: article.System.Codename, limit: 2, contentType: article.System.Type)
-                        //.WithFilterQuery("\"personas=Barista\" in 'properties'")
-                        //.WithBoosterQuery($"if 'lastupdated' >= now() - {lastMonth} then 2 else 1")
-                        .Execute();
-
-                    var articles = (await client.GetItemsAsync<Article>(new InFilter("system.codename", recommendedArticles.Select(a => a.Codename).ToArray()))).Items;
-                    article.RelatedArticles = articles.Select(a => (object) a);
-                    return View(article);
+                    }
+                    else
+                    {
+                        return View(article);
+                    }
                 }
                 catch (Exception)
                 {
