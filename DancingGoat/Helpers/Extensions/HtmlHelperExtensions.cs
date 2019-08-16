a81166283cc3da2a8b3e256f032035679ad40d6c
Anastasios Moraitis <anas.moraitis@gmail.com>
2019-08-16 23:33:04 +0300

    Create Asp.Net Core 2.2 project and merge/refactor some of the existing functionalities

--- DancingGoat/Helpers/Extensions/HtmlHelperExtensions.cs
+++ DancingGoat/Helpers/Extensions/HtmlHelperExtensions.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Configuration;
+﻿using DancingGoat.Models;
+using KenticoCloud.ContentManagement.Helpers.Models;
+using KenticoCloud.Delivery.ImageTransformation;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Web.Mvc;
-using System.Web.Mvc.Html;
-using DancingGoat.Areas.Admin;
-using DancingGoat.Models;
-
-using KenticoCloud.ContentManagement.Helpers.Models;
-using KenticoCloud.Delivery;
-using KenticoCloud.Delivery.ImageTransformation;
+using System.Net.Http;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace DancingGoat.Helpers.Extensions
 {
     public static class HtmlHelperExtensions
     {
+
         /// <summary>
         /// Generates an IMG tag for an image file.
         /// </summary>
+        /// <remarks>Should replace it with the asp.net core's img TagHelper in each view</remarks>
         /// <param name="htmlHelper">HTML helper.</param>
         /// <param name="asset">Asset</param>
         /// <param name="title">Title</param>
@@ -25,13 +29,13 @@ namespace DancingGoat.Helpers.Extensions
         /// <param name="width">Optional width size</param>
         /// <param name="height">Optional height size</param>
         /// <param name="sizes">Media conditions mapping screen width to image size</param>
-        public static MvcHtmlString AssetImage(this HtmlHelper htmlHelper, Asset asset, string title = null, string cssClass = "", int? width = null, int? height = null, ResponsiveImageSizes sizes = null)
+        public static IHtmlContent AssetImage(this IHtmlHelper htmlHelper, IConfiguration configuration, KenticoCloud.Delivery.Asset asset, string title = null, string cssClass = "", int? width = null, int? height = null, ResponsiveImageSizes sizes = null)
         {
             if (asset == null)
             {
-                return MvcHtmlString.Empty;
+                return new StringHtmlContent(string.Empty);
             }
-
+            
             var imageUrlBuilder = new ImageUrlBuilder(asset.Url);
             var image = new TagBuilder("img");
 
@@ -46,10 +50,10 @@ namespace DancingGoat.Helpers.Extensions
                 image.MergeAttribute("height", height.ToString());
                 imageUrlBuilder = imageUrlBuilder.WithHeight(Convert.ToDouble(height));
             }
-
-            if (AppSettingProvider.ResponsiveImagesEnabled && !width.HasValue && !height.HasValue)
+            bool.TryParse(configuration.GetSection("AppConfiguration")["ResponsiveImagesEnabled "], out bool ResponsiveImagesEnabled);
+            if (ResponsiveImagesEnabled && !width.HasValue && !height.HasValue)
             {
-                image.MergeAttribute("srcset", GenerateSrcsetValue(asset.Url));
+                image.MergeAttribute("srcset", GenerateSrcsetValue(asset.Url, configuration));
 
                 if (sizes != null)
                 {
@@ -62,8 +66,16 @@ namespace DancingGoat.Helpers.Extensions
             string titleToUse = title ?? asset.Description ?? string.Empty;
             image.MergeAttribute("alt", titleToUse);
             image.MergeAttribute("title", titleToUse);
+            image.TagRenderMode = TagRenderMode.SelfClosing;
 
-            return MvcHtmlString.Create(image.ToString(TagRenderMode.SelfClosing));
+            string result;
+            using (var writer = new StringWriter())
+            {
+                image.WriteTo(writer, HtmlEncoder.Default);
+                result = writer.ToString();
+            }
+
+            return new HtmlString(result);
         }
 
         /// <summary>
@@ -72,18 +84,18 @@ namespace DancingGoat.Helpers.Extensions
         /// <param name="htmlHelper">HTML helper.</param>
         /// <param name="image">Inline image.</param>
         /// <param name="sizes">Media conditions mapping screen width to image size</param>
-        public static MvcHtmlString InlineImage(this HtmlHelper htmlHelper, IInlineImage image, ResponsiveImageSizes sizes = null)
+        public static IHtmlContent InlineImage(this IHtmlHelper htmlHelper, IConfiguration configuration, KenticoCloud.Delivery.IInlineImage image, ResponsiveImageSizes sizes = null)
         {
             if (image == null)
             {
-                return MvcHtmlString.Empty;
+                return new StringHtmlContent(string.Empty);
             }
 
             var imageTag = new TagBuilder("img");
-
-            if (AppSettingProvider.ResponsiveImagesEnabled)
+            bool.TryParse(configuration.GetSection("AppConfiguration")["ResponsiveImagesEnabled"], out bool ResponsiveImagesEnabled);
+            if (ResponsiveImagesEnabled)
             {
-                imageTag.MergeAttribute("srcset", GenerateSrcsetValue(image.Src));
+                imageTag.MergeAttribute("srcset", GenerateSrcsetValue(image.Src, configuration));
 
                 if (sizes != null)
                 {
@@ -93,8 +105,17 @@ namespace DancingGoat.Helpers.Extensions
 
             imageTag.MergeAttribute("src", image.Src);
             imageTag.MergeAttribute("alt", image.AltText);
+            imageTag.TagRenderMode = TagRenderMode.SelfClosing;
 
-            return MvcHtmlString.Create(imageTag.ToString(TagRenderMode.SelfClosing));
+            string result;
+
+            using (var writer = new StringWriter())
+            {
+                imageTag.WriteTo(writer, HtmlEncoder.Default);
+                result = writer.ToString();
+            }
+            
+            return new HtmlString(result);
         }
 
         /// <summary>
@@ -104,81 +125,9 @@ namespace DancingGoat.Helpers.Extensions
         /// <param name="expression">The expression of the model property</param>
         /// <param name="format">The formatting character</param>
         /// <remarks>The TValue generic parameter is chosen instead of DateTime just to save views from falling to exceptions. With TValue, the views will get rendered, only this helper method will return an empty <see cref="MvcHtmlString"/>.</remarks>
-        public static MvcHtmlString DateTimeFormattedFor<TModel>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, DateTime?>> expression, string format)
+        public static IHtmlContent DateTimeFormattedFor<TModel>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, DateTime?>> expression, string format)
         {
             return htmlHelper.DisplayFor(expression, "DateTime", new DateTimeFormatterParameters { FormatCharacter = format });
-        }
-
-        /// <summary>
-        /// Returns an HTML input element with a label and validation fields for each property in the object that is represented by the <see cref="Expression"/> expression.
-        /// </summary>
-        /// <typeparam name="TModel">The type of the model.</typeparam>
-        /// <typeparam name="TValue">The type of the value.</typeparam>
-        /// <param name="html">The HTML helper instance that this method extends.</param>
-        /// <param name="expression">An expression that identifies the object that contains the displayed properties.</param>
-        /// <param name="explanationText">An explanation text describing usage of the rendered field.</param>
-        public static MvcHtmlString ValidatedEditorFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, string explanationText = "", string id = "")
-        {
-            string label;
-            string editor;
-
-            if (!string.IsNullOrEmpty(id))
-            {
-                label = html.LabelFor(expression, new { @for = id }).ToString();
-                editor = html.EditorFor(expression, new { id }).ToString();
-            }
-            else
-            {
-                editor = html.EditorFor(expression).ToString();
-                label = html.LabelFor(expression).ToString();
-            }
-
-            var message = html.ValidationMessageFor(expression).ToString();
-            var explanationTextHtml = "";
-
-            if (!string.IsNullOrEmpty(explanationText))
-            {
-                explanationTextHtml = "<div class=\"explanation-text\">" + explanationText + "</div>";
-            }
-
-            var generatedHtml = $@"
-<div class=""form-group"">
-    <div class=""form-group-label"">{label}</div>
-    <div class=""form-group-input"">{editor}
-       {explanationTextHtml}
-    </div>
-    <div class=""message-validation"">{message}</div>
-</div>";
-
-            return MvcHtmlString.Create(generatedHtml);
-        }
-
-        public static MvcHtmlString StyledCheckBoxFor<TModel>(this HtmlHelper<TModel> html, Expression<Func<TModel, bool>> expression, object htmlAttributes, string labelText)
-        {
-            var checkBox = html.CheckBoxFor(expression, htmlAttributes).ToString();
-            var label = html.LabelFor(expression, labelText);
-            var generatedHtml = $@"
-<div class=""styled-checkbox"">
-    {checkBox}
-    <span></span>
-    {label}
-</div>";
-
-            return MvcHtmlString.Create(generatedHtml);
-        }
-
-        public static MvcHtmlString StyledRadioButtonFor<TModel, TProperty>(this HtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, object value, object htmlAttributes, string labelText)
-        {
-            var radioButton = html.RadioButtonFor(expression, value, htmlAttributes).ToString();
-            var label = html.LabelFor(expression, labelText, new { @class = "visible" });
-            var generatedHtml = $@"
-<div class=""styled-radio"">
-    {radioButton}
-    <span></span>
-    {label}
-</div>";
-
-            return MvcHtmlString.Create(generatedHtml);
         }
 
         /// <summary>
@@ -187,13 +136,14 @@ namespace DancingGoat.Helpers.Extensions
         /// <param name="htmlHelper">HTML helper</param>
         /// <param name="language">Codename of language variant.</param>
         /// <param name="elementIdentifiers">Identifiers of hierarchy of content item.</param>
-        public static MvcHtmlString BlockElementEditLink(
-            this HtmlHelper htmlHelper,
+        public static IHtmlContent BlockElementEditLink(
+            this IHtmlHelper htmlHelper,
+            IConfiguration configuration,
             string language,
             params ElementIdentifier[] elementIdentifiers
             )
         {
-            var itemUrl = GetItemElementUrl(language, elementIdentifiers);
+            var itemUrl = GetItemElementUrl(configuration, language, elementIdentifiers);
 
             var generatedHtml = $@"
 <a target=""_blank"" class=""edit-link__overlay--block"" href=""{itemUrl}"" >
@@ -202,7 +152,7 @@ namespace DancingGoat.Helpers.Extensions
   </span>
 </a>";
 
-            return MvcHtmlString.Create(generatedHtml);
+            return new HtmlString(generatedHtml);
         }
 
         /// <summary>
@@ -211,22 +161,22 @@ namespace DancingGoat.Helpers.Extensions
         /// <param name="htmlHelper">HTML helper</param>
         /// <param name="language">Codename of language variant.</param>
         /// <param name="elementIdentifiers">Identifiers of hierarchy of content item.</param>
-        public static MvcHtmlString InlineElementEditLink(
-            this HtmlHelper htmlHelper, 
-            string language, 
+        public static IHtmlContent InlineElementEditLink(
+            this IHtmlHelper htmlHelper,
+            IConfiguration configuration,
+            string language,
             params ElementIdentifier[] elementIdentifiers
             )
         {
-            var itemUrl = GetItemElementUrl(language, elementIdentifiers);
+            var itemUrl = GetItemElementUrl(configuration, language, elementIdentifiers);
 
             var generatedHtml = $@"
 <a target=""_blank"" class=""edit-link__overlay--inline"" href=""{itemUrl}"">
     <i aria-hidden=""true"" class=""edit-link__button-icon edit-link__button-icon--inline""></i>
 </a>";
 
-            return MvcHtmlString.Create(generatedHtml);
+            return new StringHtmlContent(generatedHtml);
         }
-
 
         /// <summary>
         /// Displays Edit Mode Panel while using preview api.
@@ -234,34 +184,34 @@ namespace DancingGoat.Helpers.Extensions
         /// <param name="htmlHelper">HTML helper</param>
         /// <param name="itemId">Id (guid) of content item identifier</param>
         /// <param name="language">Codename of language variant</param>
-        public static void EditPanel(this HtmlHelper htmlHelper, string itemId, string language)
+        public static async Task EditPanelAsync(this IHtmlHelper htmlHelper, IConfiguration configuration, string itemId, string language)
         {
-            bool.TryParse(ConfigurationManager.AppSettings["UsePreviewApi"], out var isPreview);
+            bool.TryParse(configuration.GetSection("AppConfiguration")["UsePreviewApi"], out var isPreview);
 
             if (isPreview)
             {
-                var itemUrl = GetItemUrl(language, itemId);
+                var itemUrl = GetItemUrl(language, itemId, configuration);
                 var editPanelViewModel = new EditPanelViewModel() { ItemUrl = itemUrl };
-                htmlHelper.RenderPartial("EditModePanel", editPanelViewModel);
+                await htmlHelper.RenderPartialAsync("EditModePanel", editPanelViewModel);
             }
         }
 
-        private static string GetItemUrl(string language, string itemId)
+        private static string GetItemUrl(string language, string itemId, IConfiguration configuration)
         {
-            return EditLinkHelper.Instance.Builder.BuildEditItemUrl(language, itemId);
+            return EditLinkHelper.GetInstance(configuration).Builder.BuildEditItemUrl(language, itemId);
         }
 
-        private static string GetItemElementUrl(string language, params ElementIdentifier[] elementIdentifiers)
+        private static string GetItemElementUrl(IConfiguration configuration, string language, params ElementIdentifier[] elementIdentifiers)
         {
-            return EditLinkHelper.Instance.Builder.BuildEditItemUrl(language, elementIdentifiers);
+            return EditLinkHelper.GetInstance(configuration).Builder.BuildEditItemUrl(language, elementIdentifiers);
         }
 
-        private static string GenerateSrcsetValue(string imageUrl)
+        private static string GenerateSrcsetValue(string imageUrl, IConfiguration configuration)
         {
             var imageUrlBuilder = new ImageUrlBuilder(imageUrl);
 
-            return string.Join(",", AppSettingProvider.ResponsiveWidths.Select(w
-                =>$"{imageUrlBuilder.WithWidth(Convert.ToDouble(w)).Url} {w}w"));
+            return string.Join(",", configuration.GetSection("AppConfiguration")["ResponsiveWidths"].Select(w
+                => $"{imageUrlBuilder.WithWidth(Convert.ToDouble(w)).Url} {w}w"));
         }
     }
 }
--- DancingGoat/Startup.cs
+++ DancingGoat/Startup.cs
@@ -1,14 +1,84 @@
-﻿using Microsoft.Owin;
-using Owin;
-
-[assembly: OwinStartup(typeof(DancingGoat.Startup))]
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Owin;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using KenticoCloud.Delivery;
 
 namespace DancingGoat
 {
-    public partial class Startup
+    public class Startup
     {
-        public void Configuration(IAppBuilder app)
+
+        public Startup(IHostingEnvironment enviroment)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(enviroment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{enviroment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDeliveryClient(Configuration);
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddRouting();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            // see if we need an extension method or not for this
+            using(var urlRewriteStreamReader = File.OpenText("UrlRewrite.xml"))
+            {
+                var options = new RewriteOptions().AddIISUrlRewrite(urlRewriteStreamReader);
+                app.UseRewriter(options);
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseMvc(routes=>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action/Index}/{id?}");
+
+                // Replacing Areas/Admin/AdminAreaRegistration.cs
+                routes.MapAreaRoute(
+                
+                    name: "Admin",
+                    areaName: "Admin_default",
+                    template: "Admin/{controller}/{action}/{id}"
+
+                );
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
