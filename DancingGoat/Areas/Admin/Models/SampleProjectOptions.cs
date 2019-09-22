04288fc956e30a0f9ec1ebdf519b11a8f6fb276b
Anastasios Moraitis <anas.moraitis@gmail.com>
2019-09-22 13:36:29 +0300

    Finish admin area migration

---
+++ DancingGoat/Areas/Admin/Models/SampleProjectOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DancingGoat.Areas.Admin.Models
+{
+    public class SampleProjectOptions
+    {
+        [JsonProperty("projectName")]
+        public string ProjectName { get; set; }
+    }
+}
---
+++ DancingGoat/Helpers/Extensions/LocalizedRouteExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing;
+
+namespace DancingGoat.Helpers.Extensions
+{
+    public static class LocalizedRouteExtensions
+    {
+
+        // TODO : Implement MapLocalizedRoute()
+        /*public static Route MapLocalizedRoute(this RouteCollection routes, string name, string url, object defaults, string language = null)
+        {
+            Route route = new Route(
+                url,
+                new RouteValueDictionary(defaults),
+                new RouteValueDictionary(new { language = new LanguageConstraint { Language = language } }),
+                new LocalizedMvcRouteHandler(LanguageClient.DEFAULT_LANGUAGE));
+
+            routes.Add(name, route);
+            return route;
+        }*/
+    }
+}
