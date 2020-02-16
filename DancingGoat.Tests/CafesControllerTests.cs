08af2688f3360b44f0c4cf0c739110f2c7f8637b
Petr Švihlík <svihlik.petr@gmail.com>
2020-02-16 20:22:07 +0100

    fixes #35 - models cleaned up and updated

---
+++ DancingGoat.Tests/CafesControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DancingGoat.Tests
+{
+    public class CafesControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
