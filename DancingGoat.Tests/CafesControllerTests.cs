55b053eb1de2ffa3b27afbb766f489334b1734b1
Petr Švihlík <rocky.intel@gmail.com>
2020-02-18 13:57:26 +0100

    moq vs fie examples

--- DancingGoat.Tests/CafesControllerTests.cs
+++ DancingGoat.Tests/CafesControllerTests.cs
@@ -1,6 +1,7 @@
 using DancingGoat.Controllers;
 using DancingGoat.Models;
 using DancingGoat.Repositories;
+using FakeItEasy;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System.Collections.Generic;
@@ -16,10 +17,18 @@ namespace DancingGoat.Tests
         public async Task Index_ReturnsAViewResult_WithTwoListsOfCafes()
         {
             // Arrange
-            var mockRepo = new Mock<ICafesRepository>();
-            mockRepo.Setup(repo => repo.GetCafes(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync(GetCafes());
-            var controller = new CafesController(mockRepo.Object);
+            ICafesRepository fakeRepo = null;
+
+            // Example of Moq
+            var moqRepo = new Mock<ICafesRepository>();
+            moqRepo.Setup(repo => repo.GetCafes(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(GetCafes());
+            fakeRepo = moqRepo.Object;
+
+            // Example of FakeItEasy
+            fakeRepo = A.Fake<ICafesRepository>();
+            A.CallTo(() => fakeRepo.GetCafes(A<string>.Ignored, A<string>.Ignored, A<string>.Ignored)).Returns(GetCafes());
+
+            var controller = new CafesController(fakeRepo);
 
             // Act
             var result = await controller.Index();
@@ -28,7 +37,7 @@ namespace DancingGoat.Tests
             var viewResult = Assert.IsType<ViewResult>(result);
             var viewModel = Assert.IsType<CafesViewModel>(viewResult.ViewData.Model);
             Assert.Single(viewModel.CompanyCafes);
-            Assert.Equal(2, viewModel.PartnerCafes.Count());
+            Assert.Equal(2, viewModel.PartnerCafes.Count());            
         }
 
         private List<Cafe> GetCafes()
