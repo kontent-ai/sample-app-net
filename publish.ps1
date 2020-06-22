194f09be563bcc2041df4d05dad75723e73f16a3
Jiri Kovar <jiris@kentico.com>
2020-06-22 13:50:45 +0200

    DEVOPS-1936 - fixing path

--- publish.ps1
+++ publish.ps1
@@ -4,4 +4,4 @@ dotnet publish DancingGoat -c Release --runtime win-x64 -o artifacts\win-x64  -p
 7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x64.zip") ($buildFolder + "\artifacts\win-x64\*.*") -r
 
 dotnet publish DancingGoat -c Release --runtime win-x86 -o artifacts\win-x86 -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
-7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x86.zip") ($buildFolder + "\artifacts\win-x64\*.*") -r
+7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x86.zip") ($buildFolder + "\artifacts\win-x86\*.*") -r
