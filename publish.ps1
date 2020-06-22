af29ee8d02afbd23bf5e954aeff843514f76d082
Jiri Kovar <jiris@kentico.com>
2020-06-22 13:50:24 +0200

    DEVOPS-1936 - fixing path + self contained

--- publish.ps1
+++ publish.ps1
@@ -3,5 +3,5 @@ $buildFolder = if ($ENV:APPVEYOR_BUILD_FOLDER -eq $null) { $PSScriptRoot } else 
 dotnet publish DancingGoat -c Release --runtime win-x64 -o artifacts\win-x64  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
 7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x64.zip") ($buildFolder + "\artifacts\win-x64\*.*") -r
 
-dotnet publish DancingGoat -c Release --runtime win-x86 -o artifacts\win-x86 -p:PublishTrimmed=true -p:PublishReadyToRun=true
+dotnet publish DancingGoat -c Release --runtime win-x86 -o artifacts\win-x86 -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
 7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x86.zip") ($buildFolder + "\artifacts\win-x64\*.*") -r
