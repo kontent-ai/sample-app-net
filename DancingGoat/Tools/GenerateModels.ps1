59599f2e38fa8b8ea1e9621420b5c6d9e1204fb2
Petr Švihlík <rocky.intel@gmail.com>
2020-07-01 19:55:43 +0200

    model generation path fixed

--- DancingGoat/Tools/GenerateModels.ps1
+++ DancingGoat/Tools/GenerateModels.ps1
@@ -1,2 +1,2 @@
 ﻿dotnet tool restore
-dotnet tool run KontentModelGenerator -p "975bf280-fd91-488c-994c-2f04416e5ee3" -o "..\Models\ContentTypes" -n "DancingGoat.Models" -g True -s True
+dotnet tool run KontentModelGenerator -p "975bf280-fd91-488c-994c-2f04416e5ee3" -o "..\DancingGoat\Models\ContentTypes" -n "DancingGoat.Models" -g True -s True
