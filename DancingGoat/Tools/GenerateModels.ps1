2809b732216fb9ae61d8f89be59dc25113a60364
Petr Švihlík <rocky.intel@gmail.com>
2020-08-17 14:03:56 +0200

    use absolute script root path with relative output path

--- DancingGoat/Tools/GenerateModels.ps1
+++ DancingGoat/Tools/GenerateModels.ps1
@@ -1,2 +1,3 @@
 ﻿dotnet tool restore
-dotnet tool run KontentModelGenerator -p "975bf280-fd91-488c-994c-2f04416e5ee3" -o "..\DancingGoat\Models\ContentTypes" -n "DancingGoat.Models" -g True -s True
+$outputFolder = $PSScriptRoot + '\..\Models\ContentTypes'
+dotnet tool run KontentModelGenerator -p "975bf280-fd91-488c-994c-2f04416e5ee3" -o $outputFolder -n "DancingGoat.Models" -g True -s True
