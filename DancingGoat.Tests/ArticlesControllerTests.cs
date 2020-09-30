e2839b37b4bd4da00d4b07a6c3afe53328843f7c
Petr Švihlík <rocky.intel@gmail.com>
2020-09-30 20:41:58 +0200

    fixed according to https://github.com/Kentico/kontent-delivery-sdk-net/pull/232

--- DancingGoat.Tests/ArticlesControllerTests.cs
+++ DancingGoat.Tests/ArticlesControllerTests.cs
@@ -24,7 +24,7 @@ namespace DancingGoat.Tests
             var config = new Mock<IConfiguration>();
 
             MockHttpMessageHandler mockHttp = new MockHttpMessageHandler();
-            mockHttp.When($"https://deliver.kontent.ai/975bf280-fd91-488c-994c-2f04416e5ee3/items?elements.url_pattern=on_roasts&depth=1&language={CultureInfo.CurrentCulture}&system.type=article")
+            mockHttp.When($"https://deliver.kontent.ai/975bf280-fd91-488c-994c-2f04416e5ee3/items?elements.url_pattern%5Beq%5D=on_roasts&depth=1&language={CultureInfo.CurrentCulture}&system.type=article")
                 .Respond("application/json", File.ReadAllText(Path.Combine(Environment.CurrentDirectory, $"on_roasts.json")));
             IDeliveryClient client = DeliveryClientBuilder.WithProjectId("975bf280-fd91-488c-994c-2f04416e5ee3").WithDeliveryHttpClient(new DeliveryHttpClient(mockHttp.ToHttpClient())).WithTypeProvider(new CustomTypeProvider()).Build();
             var factory = new Mock<IDeliveryClientFactory>();
