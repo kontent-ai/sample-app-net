178299e6e797193339e8ce72de36372aabadf199
Petr Švihlík <rocky.intel@gmail.com>
2021-03-01 12:38:24 +0100

    build artifacts for azure devops

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
-$buildFolder = if ($ENV:APPVEYOR_BUILD_FOLDER -eq $null) { $PSScriptRoot } else { $ENV:APPVEYOR_BUILD_FOLDER }
+dotnet publish .\DancingGoat -c Release --runtime win-x64 -o .\artifacts\win-x64  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
+Compress-Archive ".\artifacts\win-x64\*.*" ".\artifacts\kontent-sample-app-net-win-x64.zip"
+Remove-Item (".\artifacts\win-x64\" + $r) -Recurse
 
-dotnet publish DancingGoat -c Release --runtime win-x64 -o artifacts\win-x64  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
-7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x64.zip") ($buildFolder + "\artifacts\win-x64\*.*") -r
-
-dotnet publish DancingGoat -c Release --runtime win-x86 -o artifacts\win-x86 -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
-7z a ($buildFolder + "\artifacts\kontent-sample-app-net-win-x86.zip") ($buildFolder + "\artifacts\win-x86\*.*") -r
+dotnet publish .\DancingGoat -c Release --runtime win-x86 -o .\artifacts\win-x86 -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained
+Compress-Archive ".\artifacts\win-x86\*.*" ".\artifacts\kontent-sample-app-net-win-x86.zip"
+Remove-Item (".\artifacts\win-x86\" + $r) -Recurse
