6846978249acb0da4f7eaa82452ede6fd32ea181
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2021-05-26 16:52:22 +0200

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
 dotnet publish .\DancingGoat -c Release --runtime win-x64 -o .\artifacts\win-x64  -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained
 Compress-Archive ".\artifacts\win-x64\" ".\artifacts\kontent-sample-app-net-win-x64.zip"
-Remove-Item (".\artifacts\win-x64\" + $r) -Recurse
+Remove-Item (".\artifacts\win-x64\" + $r) -Recurse -Force
 
 dotnet publish .\DancingGoat -c Release --runtime win-x86 -o .\artifacts\win-x86 -p:PublishTrimmed=true --self-contained
 Compress-Archive ".\artifacts\win-x86\" ".\artifacts\kontent-sample-app-net-win-x86.zip"
-Remove-Item (".\artifacts\win-x86\" + $r) -Recurse
+Remove-Item (".\artifacts\win-x86\" + $r) -Recurse -Force
