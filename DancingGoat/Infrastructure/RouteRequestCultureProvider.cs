874087e129767fc937c71500fea56fc6c55061b5
Ivan Kiral <ivanK@kentico.com>
2022-09-26 10:26:37 +0200

    Use only supported cultures otherwise throw 404

--- DancingGoat/Infrastructure/RouteRequestCultureProvider.cs
+++ DancingGoat/Infrastructure/RouteRequestCultureProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
@@ -7,12 +8,25 @@ namespace DancingGoat.Infrastructure;
 
 public class RouteRequestCultureProvider: RequestCultureProvider
 {
+    private string _lastlyUsedCulture = "en-US";
+    private readonly List<string> _supportedCultures = new() { "en-US", "es-ES" };
 
     public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
     {
         var path = httpContext.Request.Path.ToString();
-        var parts = path.Split('/');
-        var culture = parts[1];
+        var culture = path.Split('/')[1];
+
+        if (culture != "" && !_supportedCultures.Contains(culture))
+        {
+            _lastlyUsedCulture = _lastlyUsedCulture == "" ? "en-US" : _lastlyUsedCulture;
+            httpContext.Response.Redirect($"/{_lastlyUsedCulture}/404");
+        }
+        else
+        {
+            _lastlyUsedCulture = culture; 
+        }
+
+        
         return Task.FromResult(new ProviderCultureResult(culture));
     }
 }
